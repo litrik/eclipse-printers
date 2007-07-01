02d8ef5c98dfe67949be72c57aad0492b8c14860
litrik <litrik@41412c38-692f-0410-9a9a-956a0864bfcd>
2007-07-01 19:06:24 +0000

    Added copyright notices. Added headers that mentions the Apache 2.0 license. Added plenty of comments. Code cleanup.

--- com.litrik.eclipse.printers/src/com/litrik/eclipse/printers/Activator.java
+++ com.litrik.eclipse.printers/src/com/litrik/eclipse/printers/Activator.java
@@ -1,54 +1,63 @@
+/**
+ Copyright 2007 Litrik De Roy
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 package com.litrik.eclipse.printers;
 
-import org.eclipse.ui.plugin.*;
 import org.eclipse.jface.resource.ImageDescriptor;
-import org.osgi.framework.BundleContext;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 /**
- * The main plugin class to be used in the desktop.
+ * The main plug-in class of plug-ins that integrate with the Eclipse platform
+ * UI.
  */
-public class Activator extends AbstractUIPlugin {
+public class Activator extends AbstractUIPlugin
+{
+	// The plug-in ID
+	public static final String PLUGIN_ID = "com.litrik.eclipse.printers";
 
-	//The shared instance.
+	// The shared instance.
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor.
 	 */
-	public Activator() {
+	public Activator()
+	{
 		plugin = this;
 	}
 
 	/**
-	 * This method is called upon plug-in activation
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-	}
-
-	/**
-	 * This method is called when the plug-in is stopped
-	 */
-	public void stop(BundleContext context) throws Exception {
-		super.stop(context);
-		plugin = null;
-	}
-
-	/**
 	 * Returns the shared instance.
+	 * 
+	 * @return the shared instance
 	 */
-	public static Activator getDefault() {
+	public static Activator getDefault()
+	{
 		return plugin;
 	}
 
 	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path.
-	 *
+	 * Returns an image descriptor for the image file at the given plug-in
+	 * relative path.
+	 * 
 	 * @param path the path
 	 * @return the image descriptor
 	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return AbstractUIPlugin.imageDescriptorFromPlugin("com.litrik.eclipse.printers", path);
+	public static ImageDescriptor getImageDescriptor(String path)
+	{
+		return AbstractUIPlugin.imageDescriptorFromPlugin(PLUGIN_ID, path);
 	}
 }
--- com.litrik.eclipse.printers/src/com/litrik/eclipse/printers/views/PrintersView.java
+++ com.litrik.eclipse.printers/src/com/litrik/eclipse/printers/views/PrintersView.java
@@ -1,10 +1,25 @@
+/**
+ Copyright 2007 Litrik De Roy
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 package com.litrik.eclipse.printers.views;
 
 import java.text.DecimalFormat;
 
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IToolBarManager;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
@@ -29,39 +44,89 @@ import org.eclipse.ui.part.ViewPart;
 import com.litrik.eclipse.printers.actions.RefreshAction;
 
 /**
- * 
+ * View displaying all known printers.
  */
-
 public class PrintersView extends ViewPart
 {
+	/**
+	 * The JFace viewer that will hold the printers.
+	 */
 	private TableViewer viewer;
 
+	/**
+	 * The action to refresh the list of printers.
+	 */
 	private Action fRefreshAction;
 
+	/**
+	 * The width of each column.
+	 */
 	private static int columnWidths[] =
 	{ 200, 60, 120, 120, 120 };
+
+	/**
+	 * The memento keys to store the width of each column.
+	 */
 	private static String columnWidthMementoKeys[] =
 	{ "MEMENTO_WIDTH_COLUMN_0", "MEMENTO_WIDTH_COLUMN_1", "MEMENTO_WIDTH_COLUMN_2", "MEMENTO_WIDTH_COLUMN_3",
 			"MEMENTO_WIDTH_COLUMN_4" };
+
+	/**
+	 * The title of each column.
+	 */
 	private static String columnTitles[] =
 	{ "Name", "DPI", "Page Size", "Printable Area", "Trim" };
 
+	/**
+	 * The content provider responsible for obtaining the actual list of known
+	 * printers.
+	 */
 	class ViewContentProvider implements IStructuredContentProvider
 	{
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer,
+		 *      java.lang.Object, java.lang.Object)
+		 */
 		public void inputChanged(Viewer v, Object oldInput, Object newInput)
-		{}
+		{
+		// Do nothing
+		}
 
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see org.eclipse.jface.viewers.IContentProvider#dispose()
+		 */
 		public void dispose()
-		{}
+		{
+		// Do nothing
+		}
 
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see org.eclipse.jface.viewers.IStructuredContentProvider#getElements(java.lang.Object)
+		 */
 		public Object[] getElements(Object parent)
 		{
+			// Get the list of known printers.
 			return Printer.getPrinterList();
 		}
 	}
 
+	/**
+	 * The label provider responsible for generating the labels for each column.
+	 */
 	class ViewLabelProvider extends LabelProvider implements ITableLabelProvider
 	{
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see org.eclipse.jface.viewers.ITableLabelProvider#getColumnText(java.lang.Object,
+		 *      int)
+		 */
 		public String getColumnText(Object obj, int index)
 		{
 			PrinterData printerData = (PrinterData) obj;
@@ -70,20 +135,25 @@ public class PrintersView extends ViewPart
 			switch (index)
 			{
 				case 0:
+					// The "Name" column.
 					text = printerData.name;
 					break;
 				case 1:
+					// The "DPI" column.
 					text = Integer.toString(printer.getDPI().x);
 					break;
 				case 2:
+					// The "Page Size" column.
 					text = pixelsToCm(printer.getBounds().width, printer.getDPI().x) + " x "
 							+ pixelsToCm(printer.getBounds().height, printer.getDPI().y);
 					break;
 				case 3:
+					// The "Printable Area" column.
 					text = pixelsToCm(printer.getClientArea().width, printer.getDPI().x) + " x "
 							+ pixelsToCm(printer.getClientArea().height, printer.getDPI().y);
 					break;
 				case 4:
+					// The "Trim" column.
 					text = pixelsToCm(printer.computeTrim(0, 0, 0, 0).x, printer.getDPI().x) + " x "
 							+ pixelsToCm(printer.computeTrim(0, 0, 0, 0).y, printer.getDPI().y);
 					break;
@@ -94,12 +164,26 @@ public class PrintersView extends ViewPart
 			return text;
 		}
 
+		/**
+		 * Convert pixels to cm
+		 * 
+		 * @param pixels the number of pixels to convert
+		 * @param dpi the DPI
+		 * @return the number of cm for the specified pixels at the specified
+		 *         DPI
+		 */
 		private String pixelsToCm(int pixels, int dpi)
 		{
 			DecimalFormat format = new DecimalFormat("########0.00");
 			return format.format(pixels * 2.54 / dpi);
 		}
 
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see org.eclipse.jface.viewers.ITableLabelProvider#getColumnImage(java.lang.Object,
+		 *      int)
+		 */
 		public Image getColumnImage(Object obj, int index)
 		{
 			return null;
@@ -107,16 +191,33 @@ public class PrintersView extends ViewPart
 
 	}
 
+	/**
+	 * The sorter responsible for sorting each column.
+	 */
 	class PrinterSorter extends ViewerSorter
 	{
+		/**
+		 * Current sort column.
+		 */
 		private int criteria;
 
+		/**
+		 * The constructor.
+		 * 
+		 * @param criteria the column to sort
+		 */
 		public PrinterSorter(int criteria)
 		{
 			super();
 			this.criteria = criteria;
 		}
 
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see org.eclipse.jface.viewers.ViewerComparator#compare(org.eclipse.jface.viewers.Viewer,
+		 *      java.lang.Object, java.lang.Object)
+		 */
 		public int compare(Viewer viewer, Object o1, Object o2)
 		{
 
@@ -130,22 +231,30 @@ public class PrintersView extends ViewPart
 			switch (criteria)
 			{
 				case 0:
+					// The "Name" column.
 					result = collator.compare(printer1.getPrinterData().name, printer2.getPrinterData().name);
 					break;
 				case 1:
+					// The "DPI" column.
 					result = printer1.getDPI().x - printer2.getDPI().x;
 					result = result < 0 ? -1 : (result > 0) ? 1 : 0;
 					break;
 				case 2:
-					result = (printer1.getBounds().width * 1000 / printer1.getDPI().x) - (printer2.getBounds().width * 1000 / printer2.getDPI().x);
+					// The "Page Size" column.
+					result = (printer1.getBounds().width * 1000 / printer1.getDPI().x)
+							- (printer2.getBounds().width * 1000 / printer2.getDPI().x);
 					result = result < 0 ? -1 : (result > 0) ? 1 : 0;
 					break;
 				case 3:
-					result = (printer1.getClientArea().width  * 1000 / printer1.getDPI().x) - (printer2.getClientArea().width  * 1000 / printer2.getDPI().x);
+					// The "Printable Area" column.
+					result = (printer1.getClientArea().width * 1000 / printer1.getDPI().x)
+							- (printer2.getClientArea().width * 1000 / printer2.getDPI().x);
 					result = result < 0 ? -1 : (result > 0) ? 1 : 0;
 					break;
 				case 4:
-					result = (printer1.computeTrim(0, 0, 0, 0).x  * 1000 / printer1.getDPI().x) - (printer2.computeTrim(0, 0, 0, 0).x  * 1000 / printer1.getDPI().x);
+					// The "Trim" column.
+					result = (printer1.computeTrim(0, 0, 0, 0).x * 1000 / printer1.getDPI().x)
+							- (printer2.computeTrim(0, 0, 0, 0).x * 1000 / printer1.getDPI().x);
 					result = result < 0 ? -1 : (result > 0) ? 1 : 0;
 					break;
 				default:
@@ -156,6 +265,11 @@ public class PrintersView extends ViewPart
 			return result;
 		}
 
+		/**
+		 * Returns the current sort column.
+		 * 
+		 * @return the current sort column
+		 */
 		public int getCriteria()
 		{
 			return criteria;
@@ -168,9 +282,8 @@ public class PrintersView extends ViewPart
 	public PrintersView()
 	{}
 
-	/**
-	 * This is a callback that will allow us to create the viewer and initialize
-	 * it.
+	/*
+	 * Create the SWT controls for this view.
 	 */
 	public void createPartControl(Composite parent)
 	{
@@ -190,7 +303,7 @@ public class PrintersView extends ViewPart
 				{
 					for (int j = 0; j < table.getColumns().length; j++)
 					{
-						if(table.getColumns()[j] == (TableColumn)(e.widget))
+						if (table.getColumns()[j] == (TableColumn) (e.widget))
 						{
 							viewer.setSorter(new PrinterSorter(j));
 							viewer.getTable().setSortColumn(viewer.getTable().getColumns()[j]);
@@ -214,27 +327,36 @@ public class PrintersView extends ViewPart
 		contributeToActionBars();
 	}
 
+	/**
+	 * Make all contributions to the action bars.
+	 */
 	private void contributeToActionBars()
 	{
 		IActionBars bars = getViewSite().getActionBars();
 		fillLocalToolBar(bars.getToolBarManager());
 	}
 
+	/**
+	 * Fill the local tool bar.
+	 * 
+	 * @param manager the tool bar manager.
+	 */
 	private void fillLocalToolBar(IToolBarManager manager)
 	{
 		manager.add(fRefreshAction);
 	}
 
+	/**
+	 * Make all the different actions.
+	 */
 	private void makeActions()
 	{
 		fRefreshAction = new RefreshAction(this);
 	}
 
-	private void showMessage(String message)
-	{
-		MessageDialog.openInformation(viewer.getControl().getShell(), "Printers", message);
-	}
-
+	/*
+	 * Initialize the view by restoring the width of each column.
+	 */
 	public void init(IViewSite site, IMemento memento) throws PartInitException
 	{
 		super.init(site, memento);
@@ -252,6 +374,11 @@ public class PrintersView extends ViewPart
 		}
 	}
 
+	/*
+	 * Save the view's state by saving the width of each column.
+	 * 
+	 * @see org.eclipse.ui.part.ViewPart#saveState(org.eclipse.ui.IMemento)
+	 */
 	public void saveState(IMemento memento)
 	{
 		super.saveState(memento);
@@ -263,17 +390,19 @@ public class PrintersView extends ViewPart
 		}
 	}
 
-	/**
-	 * Passing the focus request to the viewer's control.
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.part.WorkbenchPart#setFocus()
 	 */
 	public void setFocus()
 	{
 		viewer.getControl().setFocus();
 	}
 
+	/**
+	 * Refresh the view's content.
+	 */
 	public void refresh()
 	{
 		viewer.refresh();
 	}
-
 }
